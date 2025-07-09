24221e99b04425a4be019388ead77a7d9ce65084
Caio Querino <155585518+CaioQuerino@users.noreply.github.com>
2025-07-09 18:22:22 -0300

    projetos node com jest e E2E

---
+++ nodejs-jest-E2E/src/operations.ts
@@ -0,0 +1,20 @@
+export class operations {
+    public sum(x: number, y: number): number {
+        return x + y
+    }
+
+    public sub(x: number, y: number): number {
+        return x - y
+    }
+
+    public mult(x: number, y: number): number {
+        return x * y
+    }
+
+    public div(x: number, y: number): number {
+        if (y === 0) {
+            throw new Error("Cannot divide by zero")
+        }
+        return x / y
+    }
+}
